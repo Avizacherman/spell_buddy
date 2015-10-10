75dd16e702c310981cbe3da62670522d43415cc9
Avi Zacherman <avizacherman@gmail.com>
2015-10-09 23:15:55 -0400

    spell names

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient
+var MongoLogger = require('mongodb').Logger
 
 var spellsDB = `mongodb://TheWizard:${process.env.SPELLPW}@ds031223.mongolab.com:31223/spell_list`
 
-router.get('/spell_descs', function(req, res){
+router.get('/spells', function(req, res){
+	MongoLogger.setLevel('debug')
+
 	MongoClient.connect(spellsDB, function(err, db){
 		if (err) res.send(err)
 
@@ -18,12 +21,31 @@ router.get('/spell_descs', function(req, res){
 	})
 })
 
-router.get('/spell_names', function(req, res){
+router.get('/spells/:id', function(req, res){
 	MongoClient.connect(spellsDB, function(err, db){
+		MongoLogger.setLevel('debug')
+
+
 		if (err) res.send(err)
 
 		var spellCollection = db.collection('spells')
-		spellCollection.find({name: 'Magic Missile'}).toArray(function(err, docs){
+		spellCollection.find({id: `${parseInt(req.params.id)}`}).toArray(function(err, docs){
+			if (err) res.send(err)
+
+			res.json(docs)
+				db.close()
+		})
+	})
+})
+
+router.get('/spell_names', function(req, res){
+	MongoClient.connect(spellsDB, function(err, db){
+		MongoLogger.setLevel('debug')
+
+		if (err) res.send(err)
+
+		var spellNames = db.collection('spell_names')
+		spellNames.find().toArray(function(err, docs){
 			if (err) res.send(err)
 
 			res.json(docs)
